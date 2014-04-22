4bd82595547e3d92626f812e0c55fc349214d87b
aapljosh <aapljosh@gmail.com>
2014-04-22 07:43:13 -0600

    Update index.js
    Testing my function in node.js

--- index.js
+++ index.js
@@ -85,8 +85,9 @@ app.delete('/sessions', function(req, res) {
 
 app.get('/view', function(req, res) {
   res = addHeaders(res);
-  view = fs.readFileSync('views/view.html', {encoding: 'ascii'})
-  result = mustache.render(view, {sessions: sessions, head: head, foot: foot})
+  result = ConvertJsonToTable(sessions, 'jsonTable', null, 'Download');
+  //view = fs.readFileSync('views/view.html', {encoding: 'ascii'})
+  //result = mustache.render(view, {sessions: sessions, head: head, foot: foot})
   res.end(result);
 });
 
@@ -108,3 +109,153 @@ app.post('*', function(req, res) {
 app.listen(8000)
 
 console.log("listening on 8000")
+
+/**********************************************************/
+
+/**
+ * JavaScript format string function
+ * 
+ */
+String.prototype.format = function()
+{
+  var args = arguments;
+
+  return this.replace(/{(\d+)}/g, function(match, number)
+  {
+    return typeof args[number] != 'undefined' ? args[number] :
+                                                '{' + number + '}';
+  });
+};
+
+function ConvertJsonToTable(parsedJson, tableId, tableClassName, linkText)
+{
+    //Patterns for links and NULL value
+    var italic = '<i>{0}</i>';
+    var link = linkText ? '<a href="{0}">' + linkText + '</a>' :
+                          '<a href="{0}">{0}</a>';
+
+    //Pattern for table                          
+    var idMarkup = tableId ? ' id="' + tableId + '"' :
+                             '';
+
+    var classMarkup = tableClassName ? ' class="' + tableClassName + '"' :
+                                       '';
+
+    var tbl = '<table border="1" cellpadding="1" cellspacing="1"' + idMarkup + classMarkup + '>{0}{1}</table>';
+
+    //Patterns for table content
+    var th = '<thead>{0}</thead>';
+    var tb = '<tbody>{0}</tbody>';
+    var tr = '<tr>{0}</tr>';
+    var thRow = '<th>{0}</th>';
+    var tdRow = '<td>{0}</td>';
+    var thCon = '';
+    var tbCon = '';
+    var trCon = '';
+
+    if (parsedJson)
+    {
+        var isStringArray = typeof(parsedJson[0]) == 'string';
+        var headers;
+
+        // Create table headers from JSON data
+        // If JSON data is a simple string array we create a single table header
+        if(isStringArray)
+            thCon += thRow.format('value');
+        else
+        {
+            // If JSON data is an object array, headers are automatically computed
+            if(typeof(parsedJson[0]) == 'object')
+            {
+                headers = array_keys(parsedJson[0]);
+
+                for (i = 0; i < headers.length; i++)
+                    thCon += thRow.format(headers[i]);
+            }
+        }
+        th = th.format(tr.format(thCon));
+        
+        // Create table rows from Json data
+        if(isStringArray)
+        {
+            for (i = 0; i < parsedJson.length; i++)
+            {
+                tbCon += tdRow.format(parsedJson[i]);
+                trCon += tr.format(tbCon);
+                tbCon = '';
+            }
+        }
+        else
+        {
+            if(headers)
+            {
+                var urlRegExp = new RegExp(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig);
+                var javascriptRegExp = new RegExp(/(^javascript:[\s\S]*;$)/ig);
+                
+                for (i = 0; i < parsedJson.length; i++)
+                {
+                    for (j = 0; j < headers.length; j++)
+                    {
+                        var value = parsedJson[i][headers[j]];
+                        var isUrl = urlRegExp.test(value) || javascriptRegExp.test(value);
+
+                        if(isUrl)   // If value is URL we auto-create a link
+                            tbCon += tdRow.format(link.format(value));
+                        else
+                        {
+                            if(value){
+                            	if(typeof(value) == 'object'){
+									if(value instanceof Array)
+									{
+										tbCon += tdRow.format(value.toString());
+									}
+                            		//for supporting nested tables
+                            		//tbCon += tdRow.format(ConvertJsonToTable(eval(value.data), value.tableId, value.tableClassName, value.linkText));
+                            	} else {
+                            		tbCon += tdRow.format(value);
+                            	}
+                                
+                            } else {    // If value == null we format it like PhpMyAdmin NULL values
+                                tbCon += tdRow.format(italic.format(value).toUpperCase());
+                            }
+                        }
+                    }
+                    trCon += tr.format(tbCon);
+                    tbCon = '';
+                }
+            }
+        }
+        tb = tb.format(trCon);
+        tbl = tbl.format(th, tb);
+
+        return tbl;
+    }
+    return null;
+}
+
+function array_keys(input, search_value, argStrict)
+{
+    var search = typeof search_value !== 'undefined', tmp_arr = [], strict = !!argStrict, include = true, key = '';
+
+    if (input && typeof input === 'object' && input.change_key_case) { // Duck-type check for our own array()-created PHPJS_Array
+        return input.keys(search_value, argStrict);
+    }
+ 
+    for (key in input)
+    {
+        if (input.hasOwnProperty(key))
+        {
+            include = true;
+            if (search)
+            {
+                if (strict && input[key] !== search_value)
+                    include = false;
+                else if (input[key] != search_value)
+                    include = false;
+            } 
+            if (include)
+                tmp_arr[tmp_arr.length] = key;
+        }
+    }
+    return tmp_arr;
+}
